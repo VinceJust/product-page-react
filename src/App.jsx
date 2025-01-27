43b86ac273cf12e16f711bb36a1791b9f53e5e08
VinJust <vpj97@outlook.de>
2025-01-27 15:30:58 +0100

    added functioning App.jsx, main.jsx and Footer.jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Footer from "./components/Footer";
+//import Header from "./components/Header";
+//import ProductList from "./components/ProductList";
+
+function App() {
+    return (
+        <div className="bg-gray-100">
+
+            <Footer />
+        </div>
+    );
+}
+export default App;
