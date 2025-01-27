721597b126655e598b43cea1a9d8e2ea3a83cc07
VinJust <vpj97@outlook.de>
2025-01-27 15:50:54 +0100

    added simple Header and product array to the ProductList

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import Footer from "./components/Footer";
-//import Header from "./components/Header";
-//import ProductList from "./components/ProductList";
+import Header from "./components/header";
+import ProductList from "./components/ProductList";
 
 function App() {
     return (
         <div className="bg-gray-100">
-
+            <Header />
+            <ProductList />
             <Footer />
         </div>
     );
---
+++ src/components/ProductList.jsx
@@ -0,0 +1,13 @@
+const products = [
+    { id: 1, name: "Nike Air Max", price: 120, available: true },
+    { id: 2, name: "Adidas UltraBoost", price: 180, available: false },
+    { id: 3, name: "Puma RS-X", price: 110, available: true },
+    { id: 4, name: "New Balance 574", price: 100, available: true },
+];
+
+
+function ProductList() {
+    
+}
+
+export default ProductList;
